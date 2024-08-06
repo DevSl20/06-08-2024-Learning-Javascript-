098eda686619257cbffe3c92b06e149ff7747cd2
Devang Salvi <devangsalvi21@gmail.com>
2024-08-06 08:09:09 +0000

    learned else if

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+const name = prompt("Enter Your Full name: ");
+
+if (name.includes(" ")) {
+    console.log("space is there");
+}
+// else if(name.includes("")){
+//     console.log("b is there");
+// }
+else {
+    console.log("space is not there");
+    
+}
+
+
